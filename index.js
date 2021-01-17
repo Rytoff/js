d7d136c181c5cf391900f6967bb44b2ab4ce61b7
Rytoff <roman.rytoff@gmail.com>
2021-01-17 10:24:05 +0200

    Task 08_01 finished

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const money = prompt('Ваш бюджет на месяц, грн.:', '')
+const money = +prompt('Ваш бюджет на месяц, грн.:', '')
 const time = prompt('Введите дату в формате YYYY-MM-DD','')
 
+const appData = {
+  budget: money,
+  timeData: time,
+  expenses: {
+    
+  },
+  optionalExpense: {
+    
+  },
+  income: [],
+  savings: false
+}
+appData.expenses[prompt('Введите обязательную стаью расходов в этом месяце:','')] = +prompt('Во сколько обойдется: грн.','')
+
+console.log(appData)
+
+const budgetPerDay = (appData.budget - appData.expenses[Object.keys(appData.expenses)]) / 30
+
+alert(`Бюджет на 1 день равен ${budgetPerDay} грн.`)
